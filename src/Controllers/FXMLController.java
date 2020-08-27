f094c7199029b98166d071a38fad6b2c53aeaa0b
Arian <arian.jamborzadeh@gmail.com>
2020-08-27 20:25:44 +0200

    FXML Tests

---
+++ src/Controllers/FXMLController.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import java.awt.*;
+
+public class FXMLController {
+
+    @FXML
+    public void buttonClicked(Event e){
+        System.out.println("Button Clicked");
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,12 +2,14 @@ package sample;
 
 import Controllers.ClientManager;
 import Controllers.DataContainerManager;
+import Controllers.FXMLController;
 import Controllers.KeyManager;
 import Data.EvaluationData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
@@ -21,20 +23,23 @@ public class Main extends Application {
 
    @Override
     public void start(Stage primaryStage) throws Exception{
-       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("The Fair Value");
-        /*primaryStage.setScene(new Scene(root, 300, 275));*/
+
 
        primaryStage.setX(100);
        primaryStage.setY(100);
        primaryStage.setWidth(600);
        primaryStage.setHeight(600);
-       FXMLLoader loader = new FXMLLoader();
-       loader.setLocation(new URL("file:/Users/Kara/IdeaProjects/TheFairValue/src/TestLayout.fxml"));
-       VBox vbox = loader.<VBox>load();
+       Button button1 = new Button("click me");
+       button1.setStyle("-fx-background-color:#0000ff"+"-fx-border-color:#0030ff");
+       VBox vbox = new VBox(button1);
+       button1.setLineSpacing(30);
        Scene scene = new Scene(vbox);
-
        primaryStage.setScene(scene);
+
+
+
 
 
        primaryStage.show();
