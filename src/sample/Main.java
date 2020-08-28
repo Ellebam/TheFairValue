c91e22affa3de76438368e4a50c8ef5bacca03f4
Arian <arian.jamborzadeh@gmail.com>
2020-08-28 18:22:21 +0200

    FXML Tests

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,16 @@ import Controllers.DataContainerManager;
 import Controllers.FXMLController;
 import Controllers.KeyManager;
 import Data.EvaluationData;
+import Data.FinancialDataObject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -26,17 +30,37 @@ public class Main extends Application {
 
         primaryStage.setTitle("The Fair Value");
 
+        TableView tableView = new TableView();
+
+       TableColumn<String, FinancialDataObject> column1 = new TableColumn<>("value");
+       column1.setCellValueFactory(new PropertyValueFactory<>("value"));
+
+       TableColumn<String, FinancialDataObject>column2 = new TableColumn<>("date");
+       column2.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+       tableView.getColumns().add(column1);
+       tableView.getColumns().add(column2);
+
+       tableView.getItems().add(new FinancialDataObject("oida",50,"3"));
+
+
 
        primaryStage.setX(100);
        primaryStage.setY(100);
        primaryStage.setWidth(600);
        primaryStage.setHeight(600);
        Button button1 = new Button("click me");
-       button1.setStyle("-fx-background-color:#0000ff"+"-fx-border-color:#0030ff");
+
+       VBox vBox = new VBox(tableView);
        VBox vbox = new VBox(button1);
+       vbox.getChildren().add(vBox);
+
        button1.setLineSpacing(30);
        Scene scene = new Scene(vbox);
+
+
        primaryStage.setScene(scene);
+
 
 
 
